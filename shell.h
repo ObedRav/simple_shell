ad13445552b819eb05caf9807db6435dc32d3012
ObedRav <obedrayo2356@gmail.com>
2022-12-21 21:33:00 +0000

    Add Shell

--- shell.h
+++ shell.h
@@ -1,4 +1,58 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#endif 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <stddef.h>
+#include <sys/stat.h>
+#include <signal.h>
+
+extern char **environ;
+
+/**
+ * struct list_path - Linked list containing PATH directories
+ * @dir: directory in path
+ * @p: pointer to next node
+ */
+typedef struct list_path
+{
+	char *dir;
+	struct list_path *p;
+} list_path;
+
+/**
+ * struct mybuild - pointer to function with corresponding buildin command
+ * @name: buildin command
+ * @func: execute the buildin command
+ */
+typedef struct mybuild
+{
+	char *name;
+	void (*func)(char **);
+} mybuild;
+
+void sig_handler(int sig_num);
+int _strlen(char *s);
+void _puts(char *str);
+int _putchar(char c);
+void _EOF(int len, char *buff);
+void _isatty(void);
+char **splitstring(char *str, const char *delim);
+void execute(char **argv);
+char *_getenv(const char *name);
+char *_strdup(char *str);
+char *concat_all(char *name, char *sep, char *value);
+void(*checkbuild(char **arv))(char **arv);
+list_path *linkpath(char *path);
+list_path *add_node_end(list_path **head, char *str);
+char *_which(char *filename, list_path *head);
+void free_list(list_path *head);
+void freearv(char **arv);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+#endif
---
+++ strings.c
@@ -0,0 +1,119 @@
+#include "shell.h"
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory, which
+ * contains a copy of the string given as a parameter
+ * @str: pointer to a string
+ * Return: pointer to a string
+ */
+char *_strdup(char *str)
+{
+	int i, l;
+	char *new;
+
+	if (!str)
+	{
+		return (NULL);
+	}
+	for (l = 0; str[l] != '\0';)
+	{
+		l++;
+	}
+	new = malloc(sizeof(char) * l + 1);
+	if (!new)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < l; i++)
+	{
+		new[i] = str[i];
+	}
+	new[l] = str[l];
+	return (new);
+}
+
+/**
+ * concat_all - concats 3 strings in a newly allocated memory
+ * @name: first string
+ * @sep: second string
+ * @value: Third string
+ * Return: pointer to the new string
+ */
+char *concat_all(char *name, char *sep, char *value)
+{
+	char *result;
+	int l1, l2, l3, i, k;
+
+	l1 = _strlen(name);
+	l2 = _strlen(sep);
+	l3 = _strlen(value);
+
+	result = malloc(l1 + l2 + l3 + 1);
+	if (!result)
+		return (NULL);
+
+	for (i = 0; name[i]; i++)
+		result[i] = name[i];
+	k = i;
+
+	for (i = 0; sep[i]; i++)
+		result[k + i] = sep[i];
+	k = k + i;
+
+	for (i = 0; value[i]; i++)
+		result[k + i] = value[i];
+	k = k + i;
+
+	result[k] = '\0';
+
+	return (result);
+}
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _strlen - count chars
+ * @s: data from user
+ * Return: The result
+ */
+int _strlen(char *s)
+{
+	int i = 0, length = 0;
+
+	while (s[i++])
+	{
+		length++;
+	}
+
+	return (length);
+}
+
+/**
+ * puts2 - prints every other character of a string, starting with the first
+ *         character, followed by a new line.
+ * @str: String
+ */
+void _puts(char *str)
+{
+	int i, length;
+
+	length = _strlen(str);
+	for (i = 0; i <= (length - 1); i++)
+	{
+		if ((i % 2) == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
